8907bb608f5b1dbf9a23ceb5f6fc387e008a1e7e
Tiffany Bibby <tiffanybibby@Tiffanys-MBP.fios-router.home>
2021-10-06 14:46:23 -0400

    updated app.js code

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,64 @@
-import logo from './logo.svg';
+import { useState, useEffect } from 'react';
+import { Switch, Route, useHistory } from 'react-router-dom';
 import './App.css';
 
+import Layout from './layouts/Layout';
+import Login from './screens/Login';
+import Register from './screens/Register';
+import MainContainer from './containers/MainContainer';
+
+import {
+  loginOwner,
+  registerOwner,
+  verifyOwner,
+  removeToken,
+} from './services/auth';
+
 function App() {
+  const [currentOwner, setCurrentOwner] = useState(null);
+  const history = useHistory();
+
+  useEffect(() => {
+    const handleVerify = async () => {
+      const ownerData = await verifyOwner();
+      setCurrentOwner(ownerData);
+    };
+    handleVerify();
+  }, []);
+
+  const handleLogin = async (loginData) => {
+    const ownerData = await loginOwner(loginData);
+    setCurrentOwner(ownerData);
+    history.push('/');
+  };
+
+  const handleRegister = async (registerData) => {
+    const ownerData = await registerOwner(registerData);
+    setCurrentOwner(ownerData);
+    history.push('/');
+  };
+
+  const handleLogout = () => {
+    setCurrentOwner(null);
+    localStorage.removeItem('authToken');
+    removeToken();
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <Layout currentOwner={currentOwner} handleLogout={handleLogout}>
+        <Switch>
+          <Route path='/login'>
+            <Login handleLogin={handleLogin} />
+          </Route>
+          <Route path='/register'>
+            <Register handleRegister={handleRegister} />
+          </Route>
+          <Route path='/'>
+            <MainContainer />
+          </Route>
+        </Switch>
+      </Layout>
     </div>
   );
 }
