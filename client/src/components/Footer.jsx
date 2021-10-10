e3b29b939c3e2e198c97c24b7c88620075320109
Tiffany Bibby <tiffanybibby@Tiffanys-MBP.fios-router.home>
2021-10-09 22:17:21 -0400

    styling

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -5,11 +5,11 @@ import { CgCopyright } from 'react-icons/cg'
 function Footer(props) {
   return (
     <div className="footer">
-        {/* <div className="footer-links"> */}
+        <div className="footer-links">
           <a className='git-icons' href="https://github.com/tiffanybibby" target="blank"> <img className="github-img" src="https://github.com/tiffanybibby/acrebound/blob/main/assets/github_logo.png?raw=true" alt='github icon' /> </a>
         <p> <CgCopyright/> Tiffany D. Bibby - 2021</p>
           <a className='linkedin' href="https://www.linkedin.com/in/tiffanybibby" target="blank"> <img className="linkedin-img" src="https://github.com/tiffanybibby/acrebound/blob/main/assets/linkedin_logo.png?raw=true" alt='linkedin icon' /> </a>
-        {/* </div> */}
+        </div>
     </div>
   );
 }
