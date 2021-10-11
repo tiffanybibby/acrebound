4f7ff5b7467c8d7422b0a1f10de99e97f6834218
Tiffany Bibby <tiffanybibby@Tiffanys-MBP.fios-router.home>
2021-10-11 04:33:01 -0400

    home page styling

--- client/src/components/Nav.jsx
+++ client/src/components/Nav.jsx
@@ -1,54 +1,56 @@
 import React, { useState } from 'react';
-import "./Nav/Nav.css"
+import "./Nav.css"
 import * as FaIcons from 'react-icons/fa';
 import * as AiIcons from 'react-icons/ai';
 import { Link } from "react-router-dom";
 import { NavLink } from 'react-router-dom'
 import { SidebarData } from './SideMenuData';
 
-function Nav(props) {
+export default function Nav(props) {
   const [sidebar, setSidebar] = useState(false);
 
   const showSidebar = () => setSidebar(!sidebar);
 
   const { currentOwner, handleLogout } = props;
 
-const authenticatedOptions = (
-  <>
-      <NavLink className="link" to="/properties/new">Add Properties</NavLink>
+  const authenticatedOptions = (
+    <>
+      <NavLink className="nav-link" to="/properties/new">Add Properties</NavLink>
+      <br />
       <button className="logout" onClick={handleLogout}>Logout</button>
-  </>
-)
-const unauthenticatedOptions = (
-  <>
-      <NavLink className="link" to="/login">Login</NavLink>
-      <NavLink className="link" to="/register">Register</NavLink>
-  </>
-)
+    </>
+  )
+  const unauthenticatedOptions = (
+    <>
+      <NavLink className="nav-link" to="/login">Login</NavLink>
+      <br />
+      <NavLink className="nav-link" to="/register">Register</NavLink>
+    </>
+  )
 
   return (
     <>
-    <div className="nav">
-      <div className='navbar'>
+      <div className="nav">
+        <div className='navbar'>
           <Link to='#' className='menu-bars'>
             <FaIcons.FaBars onClick={showSidebar} />
           </Link>
           <div className="acrebound-logo">
             <Link to="/">
               <div>
-            <img
-              className="logo-img"
-              src="https://i.imgur.com/51pfGRS.png"
-              alt="Acrebound logo icon"
-            />
+                <img
+                  className="logo-img"
+                  src="https://i.imgur.com/51pfGRS.png"
+                  alt="Acrebound logo icon"
+                />
               </div>
-          </Link>
-        </div>
+            </Link>
+          </div>
           <div className="nav-options">
-          {currentOwner && <NavLink className="hello-owner" to="/properties"> Hi, {currentOwner.first_name}</NavLink>}
-          {currentOwner ? authenticatedOptions : unauthenticatedOptions}     
-        
- </div>
+            {currentOwner && <NavLink className="hello-owner" to="/properties"> Hi, {currentOwner.first_name}</NavLink>}
+            <br />
+            {currentOwner ? authenticatedOptions : unauthenticatedOptions}
+          </div>
 
 
 
@@ -73,10 +75,7 @@ const unauthenticatedOptions = (
             })}
           </ul>
         </nav>
-        </div>
+      </div>
     </>
   );
 }
-
-
-export default Nav;
--- client/src/screens/Home/Home.jsx
+++ client/src/screens/Home/Home.jsx
@@ -4,16 +4,33 @@ import { ImLeaf } from 'react-icons/im';
 
 const Home = (props) => {
   return (
+    <>
+      <header></header>
       <div className="home">
-      <div className="join-message">
-        <p>For the saavy real estate investor <ImLeaf className="message-leaf"/>
-          <br />
-          <br/>
-Keep track of your properties with AcreBound</p>
-      <br></br>
-      <Link to='/register'><button className="register-btn">Register</button></Link>
+        <div className="house" >
+          {/* <img className="house-gif" src="https://media.giphy.com/media/l0IylQoMkcbZUbtKw/giphy.gif" alt="house" /> */}
+          {/* <img className="house-gif" src="https://connectedinvestors.com/blog/wp-content/uploads/2018/06/The-Commercial-Real-Estate-Investors-Hedge-Against-Recession-.gif" alt="house" /> */}
+          {/* <img className="house-gif" src="https://im2.ezgif.com/tmp/ezgif-2-a43df0ecd442.gif" alt="house" /> */}
+          <img className="house-gif" src="https://github.com/tiffanybibby/acrebound/blob/main/assets/house.gif?raw=true" alt="house gif" />
+          {/* <img className="house-gif" src="https://cdn.dribbble.com/users/1423769/screenshots/4451874/preloader-03-04-dribbble.gif" alt="house" /> */}
+
+
+        </div>
+        <div className="join-message">
+          <div className="message">
+            <p>Experience a fresh way to manage your<br />
+              <span className="emphasis">real estate</span><br /><br />
+              <span className="emphasis2">AcreBound <ImLeaf className="message-leaf" /></span> </p>
+            <br />
+            <Link to='/register'><button className="register-btn">Sign up for AcreBound</button></Link>
+            <p className="subtext">Reach your goals with personalized insights,<br />
+              organize assets in one secure central hub, <br />
+              increase efficiency & mitigate risk</p>
+          </div>
         </div>
       </div>
+      <footer></footer>
+    </>
   );
 };
 
